3333dc486792dd9d292bac73ed52d7c010289447
Logan <logan.life.online@gmail.com>
2020-01-18 16:37:07 -0500

    feat: random image tweets
    The bot will now tweet one Penn image per day from a directory of images.

--- professor59x.py
+++ professor59x.py
@@ -1,55 +1,89 @@
 import tweepy
 from credentials import *
 from time import sleep
+import glob
+import random
+import os, sys
 
 # setup OAuth and integrate with API
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+# open the text file
+my_file = open('AMND.txt', 'r')
+
+# read line by line and store
+file_lines = my_file.readlines()
+
+# close the file
+my_file.close()
+
+
+# tweet_image(/images)
+
+
+def from_image():
+    path = "/Users/logan/code/src/Environments/twitter/Professor_59X/images"
+    _files = os.listdir(path)
+    number = random.randint(0, len(_files) - 1)
+    file_ = _files[number]
+    image_name = path+"/"+file_
+    res = api.media_upload(image_name)
+    media_id = []
+    media_id.append(res.media_id)
+    print(media_id)
+    api.update_status(status = "Daily Penn Pic! #Penn", media_ids = media_id)
+    sleep(86400)
+
 
 def from_text():
-    # open the text file
-    my_file = open('verne.txt', 'r')
-
-    # read line by line and store
-    file_lines = my_file.readlines()
-
-    # close the file
-    my_file.close()
-
     for line in file_lines:
-        try:
-            print(line)
+        line = line.strip()
+        # print("::"+line+"::")
 
-            if line != '\n':
-                api.update_status(line)
-            else:
-                pass
-        except tweepy.TweepError as e:
-            print(e.reason)
+        CHARACTERS = ('LYSANDER', 'DEMETRIUS', 'HERMIA', 'HELENA',
+                      'OBERON', 'TITANIA', 'PUCK', 'ROBIN GOODFELLOW')
+        ENDINGS = ('.', '!', '?')
+        if line.startswith(CHARACTERS) and line.endswith(ENDINGS):
+
+            try:
+                print(line)
+                # if line != '\n':
+                #   api.update_status(line)
+                # else:
+                #   pass
+            except tweepy.TweepError as e:
+                print(e.reason)
+
+        # sleep(2)
+
+
+def from_hashtag():
+    for tweet in tweepy.Cursor(api.search, q='#PennEngineering').items():
+        try:
+            print('\nTweet by: @' + tweet.user.screen_name)
+
+            tweet.retweet()
+            print('Retweeted the tweet')
+
+            tweet.favorite()
+            print('Favorited the tweet')
+
+            if not tweet.user.following:
+                tweet.user.follow()
+                print('Followed the user')
+
+            # wait for an hour before retweeting anything else
             sleep(3600)
 
+        except tweepy.TweepError as e:
+            print(e.reason)
 
-for tweet in tweepy.Cursor(api.search, q='#PennEngineering').items():
-    try:
-        print('\nTweet by: @' + tweet.user.screen_name)
+        except StopIteration:
+            break
 
-        tweet.retweet()
-        print('Retweeted the tweet')
 
-        tweet.favorite()
-        print('Favorited the tweet')
-
-        if not tweet.user.following:
-            tweet.user.follow()
-            print('Followed the user')
-
-        #wait for an hour before retweeting anything else
-        sleep(3600)
-
-    except tweepy.TweepError as e:
-        print(e.reason)
-
-    except StopIteration:
-        break
+# from_text()
+while True:
+    from_image()
