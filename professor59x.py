5e4e0290eb877029e197854f3b25e72ff3731626
Logan <logan.life.online@gmail.com>
2020-01-19 11:55:25 -0500

    feat: heroku webapp setup
    Initial build for deploying on heroku.

--- professor59x.py
+++ professor59x.py
@@ -5,6 +5,12 @@ import random
 import os
 import sys
 import threading
+from os import environ
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
+
 
 # setup OAuth and integrate with API
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
