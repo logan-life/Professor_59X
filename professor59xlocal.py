b39c4c44bc610704fa4b8ef85ab3944b1f97429e
Logan <logan.life.online@gmail.com>
2020-01-19 15:15:26 -0500

    chore: removed environs from local version

--- professor59xlocal.py
+++ professor59xlocal.py
@@ -6,10 +6,7 @@ import os
 import sys
 import threading
 from os import environ
-CONSUMER_KEY = environ['CONSUMER_KEY']
-CONSUMER_SECRET = environ['CONSUMER_SECRET']
-ACCESS_KEY = environ['ACCESS_KEY']
-ACCESS_SECRET = environ['ACCESS_SECRET']
+
 
 
 # setup OAuth and integrate with API
