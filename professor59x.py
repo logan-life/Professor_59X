7782475189c22c683e265b2fbe0b252583707401
Logan <logan.life.online@gmail.com>
2021-07-07 12:14:30 -0400

    misc

--- professor59x.py
+++ professor59x.py
@@ -25,91 +25,87 @@ api = tweepy.API(auth)
 
 
 def from_image():
-    while True:
-        try:
-            path = "images"
-            _files = os.listdir(path)
-            number = random.randint(0, len(_files) - 1)
-            file_ = _files[number]
-            image_name = path+"/"+file_
-            res = api.media_upload(image_name)
-            media_id = []
-            media_id.append(res.media_id)
-            print(media_id)
-            api.update_status(status="Enjoy this Penn Pic! #Penn #UPenn #universityofpennsylvania #photography",
-                              media_ids=media_id)
-            # wait one day in between
-            sleep(HOUR * 24)
+    try:
+        path = "images"
+        _files = os.listdir(path)
+        number = random.randint(0, len(_files) - 1)
+        file_ = _files[number]
+        image_name = path+"/"+file_
+        res = api.media_upload(image_name)
+        media_id = []
+        media_id.append(res.media_id)
+        print(media_id)
+        api.update_status(status="Enjoy this Penn Pic! #Penn #UPenn #universityofpennsylvania #photography",
+                          media_ids=media_id)
+        # wait one day in between
+        sleep(HOUR * 24)
 
-        except tweepy.TweepError as e:
-            print(e.reason)
-            sleep(HOUR)  # if hit an error, try again in an hour
+    except tweepy.TweepError as e:
+        print(e.reason)
+        sleep(HOUR)  # if hit an error, try again in an hour
 
 
 # searches twitter for recent usage of supplied hashtag, then RTs / favs / follows the user who tweeted something using the hashtag.
 
 
 def from_hashtag():
-    while True:
-        for tweet in tweepy.Cursor(api.search, q='#PennEngineering').items():
-            try:
-                print('\nTweet by: @' + tweet.user.screen_name)
 
-                tweet.retweet()
-                print('Retweeted the tweet')
+    for tweet in tweepy.Cursor(api.search, q='#PennEngineering').items():
+        try:
+            print('\nTweet by: @' + tweet.user.screen_name)
 
-                tweet.favorite()
-                print('Favorited the tweet')
+            tweet.retweet()
+            print('Retweeted the tweet')
 
-                if not tweet.user.following:
-                    tweet.user.follow()
-                    print('Followed the user')
+            tweet.favorite()
+            print('Favorited the tweet')
 
-                # wait for three hours before trying again
-                sleep(HOUR * 3)
+            if not tweet.user.following:
+                tweet.user.follow()
+                print('Followed the user')
 
-            except tweepy.TweepError as e:
-                print(e.reason)
-                sleep(HOUR)
+            # wait for three hours before trying again
+            sleep(HOUR * 3)
 
-            except StopIteration:
-                break
+        except tweepy.TweepError as e:
+            print(e.reason)
+            sleep(HOUR)
+
+        except StopIteration:
+            break
 
 # checks if there are any new tweets from the supplier user @PennEngineers and RTs them if so.
 
 
 def from_user():
-    while True:
-        for tweet in tweepy.Cursor(api.user_timeline, id=418824121).items(2):
-            try:
-                # Add \n escape character to print() to organize tweets
-                print('\nTweet by: @' + tweet.user.screen_name)
+    for tweet in tweepy.Cursor(api.user_timeline, id=418824121).items(2):
+        try:
+            # Add \n escape character to print() to organize tweets
+            print('\nTweet by: @' + tweet.user.screen_name)
 
-                # Retweet and favorite tweets as they are found
-                tweet.retweet()
-                print('Retweeted the tweet')
+            # Retweet and favorite tweets as they are found
+            tweet.retweet()
+            print('Retweeted the tweet')
 
-                tweet.favorite()
-                print('Favorited the tweet')
+            tweet.favorite()
+            print('Favorited the tweet')
 
-                # wait three hours before trying again
-                sleep(HOUR * 3)
+            # wait three hours before trying again
+            sleep(HOUR * 3)
 
-            except tweepy.TweepError as e:
-                print(e.reason)
-                sleep(HOUR)
-            except StopIteration:
-                break
+        except tweepy.TweepError as e:
+            print(e.reason)
+            sleep(HOUR)
+        except StopIteration:
+            break
 
 
 # multi-threaded so that each function can have different sleep times.
 # This might be overkill but it was easy.
 t1 = threading.Thread(target=from_image)
-#t2 = threading.Thread(target=from_text)
-t3 = threading.Thread(target=from_hashtag)
-t4 = threading.Thread(target=from_user)
+t2 = threading.Thread(target=from_hashtag)
+t3 = threading.Thread(target=from_user)
 
 t1.start()
-
+t2.start()
 t3.start()
-t4.start()
